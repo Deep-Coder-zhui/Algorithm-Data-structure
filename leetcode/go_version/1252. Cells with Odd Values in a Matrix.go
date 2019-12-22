e01d87cf684323bbe0ce6a3119a85fc3b8b08c36
Deep-Coder-zhui <1733597849@qq.com>
2019-12-22 20:10:34 +0800

    two

--- leetcode/go_version/1252. Cells with Odd Values in a Matrix.go
+++ leetcode/go_version/1252. Cells with Odd Values in a Matrix.go
@@ -2,8 +2,6 @@
 常规方法：时间：O((n + m)*len(indices))，空间：O(n*m)，就是对每个影响的元素进行add 1操作，然后找出奇数个数
 优化：时间：O(len(indices) + n*m)，空间：O(n + m)，统计indices中行、列出现次数，找出所有row[i]+col[j]中的奇数
 */
-// package go_version
-
 func oddCells(n int, m int, indices [][]int) int {
 	// row, col := make([]int, n), make([]int, m) ----4ms
 	row, col := [55]int{}, [55]int{}            //----0ms
